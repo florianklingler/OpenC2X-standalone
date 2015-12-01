0eb70e80f7c22234620f393abf2c32a67882e585
Sven Laux <slaux@mail.uni-paderborn.de>
2015-12-01 16:14:52 +0100

    added threads to cam

--- cam/src/cam.h
+++ cam/src/cam.h
@@ -1,5 +1,22 @@
-class CAM
-{
+#include <zmq.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>
+
+class CAM {
 public:
-  void loop();
+	CAM ();
+	~CAM ();
+	
+	void init();
+	void receiveLoopFromDCC();
+	void sendLoop();
+
+private:
+	zmq::context_t* context;
+	zmq::socket_t* publisher_dcc;
+	zmq::socket_t* publisher_ldm;
+	zmq::socket_t* subscriber_dcc;
+
+	boost::thread* receiveFromDCCThread;
+	boost::thread* sendThread;
 };
