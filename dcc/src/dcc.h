4d15f01462ac36f6a9166865f3728f48565b75d7
Stefan Schneider <stschn@mail.upb.de>
2015-12-01 15:18:50 +0100

    Introduced threads in the DCC

--- dcc/src/dcc.h
+++ dcc/src/dcc.h
@@ -1,16 +1,23 @@
 #include <zmq.hpp>
-class DCC
-{
+#include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>
+
+class DCC {
 public:
-  DCC ();
-
-  ~DCC ();
-
-  void
-  loop ();
+	DCC ();
+	~DCC ();
+	
+	void init();
+	void receiveLoopFromUpper();
+	void receiveLoopFromLower();
 
 private:
-  zmq::context_t* context;
-  zmq::socket_t* publisher;
+	zmq::context_t* context;
+	zmq::socket_t* publisher_up;
+	zmq::socket_t* publisher_down;
+	zmq::socket_t* subscriber_up;
+	zmq::socket_t* subscriber_down;
 
+	boost::thread* receiveFromUpperThread;
+	boost::thread* receiveFromLowerThread;
 };
