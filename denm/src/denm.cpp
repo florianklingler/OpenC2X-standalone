25d12c0f63a5faf56d2b859fd67b8714aaf658a6
Gurjashan S. Pannu <gspannu@mail.uni-paderborn.de>
2015-12-01 16:14:29 +0100

    Add threads in DENM

--- denm/src/denm.cpp
+++ denm/src/denm.cpp
@@ -10,21 +10,34 @@
 using namespace std;
 using namespace zmq;
 
-void DENM::loop () {
-	context_t context (1);
+DENM::DENM () {
+	context = new zmq::context_t(1);
   	//subscriber for receiving DENMs from DCC
-  	socket_t subscriber_dcc (context, ZMQ_SUB);
-	subscriber_dcc.connect ("tcp://localhost:5555");
-  	subscriber_dcc.setsockopt ( ZMQ_SUBSCRIBE, "DENM", 1);
+  	subscriber_dcc = new zmq::socket_t(*context, ZMQ_SUB);
+	subscriber_dcc->connect ("tcp://localhost:5555");
+  	subscriber_dcc->setsockopt ( ZMQ_SUBSCRIBE, "DENM", 1);
   
   	//publisher for sending DENMs to DCC
-  	socket_t publisher_dcc(context, ZMQ_PUB);
-  	publisher_dcc.bind("tcp://*:7777");
+  	publisher_dcc = new zmq::socket_t(*context, ZMQ_PUB);
+  	publisher_dcc->bind("tcp://*:7777");
 
   	//publisher for sending DENMs to LDM
-	socket_t publisher_ldm(context, ZMQ_PUB);
-	publisher_ldm.bind("tcp://*:9999");
+	publisher_ldm = new zmq::socket_t(*context, ZMQ_PUB);
+	publisher_ldm->bind("tcp://*:9999");
+}
 
+DENM::~DENM() {
+	mSendToDccThread->join();
+	mReceiveFromDccThread->join();
+}
+
+void DENM::init() {
+	mSendToDccThread = new boost::thread(&DENM::sendToDccLoop, this);
+	mReceiveFromDccThread = new boost::thread(&DENM::receiveFromDccLoop, this);
+}
+
+void DENM::loop() {
+	/*
   	//variables
   	string topic;	
   	string msg_str;
@@ -47,6 +60,7 @@ void DENM::loop () {
 		cout << "Sending DENM to DCC and LDM" << endl;
 		s_sendmore(publisher_dcc, "DENM");
 		s_send(publisher_dcc, msg_str);
+		
 		//Send DENM to LDM
 		s_sendmore(publisher_ldm, "DENM");
 		s_send(publisher_ldm, msg_str);
@@ -60,6 +74,7 @@ void DENM::loop () {
         	msg_denm_recv.ParseFromString(msg_str);
         	google::protobuf::TextFormat::PrintToString(msg_denm_recv, &text_str);
         	cout << text_str << endl;
+        	
 		//Forward DENM to LDM
 		cout << "Forwarding DENM to LDM" << endl;
 		s_sendmore(publisher_ldm, topic);
@@ -67,11 +82,66 @@ void DENM::loop () {
 
 		sleep(3);
   	}
+  	*/
+}
+
+
+void DENM::receiveFromDccLoop() {
+	//variables
+  	string topic;	
+  	string msg_str;
+  	string text_str;
+  	
+  	GOOGLE_PROTOBUF_VERIFY_VERSION;
+  	buffers::DENM msg_denm_recv;
+  	
+	while(1) {
+		cout<<"Receiving DENM from DCC"<<endl;
+		//Receive DENM from DCC
+		topic = s_recv(*subscriber_dcc);
+		msg_str = s_recv(*subscriber_dcc);
+		cout << "Received DENM from DCC" << endl;
+		msg_denm_recv.ParseFromString(msg_str);
+		google::protobuf::TextFormat::PrintToString(msg_denm_recv, &text_str);
+		cout << text_str << endl;
+				
+		//Forward DENM to LDM
+		cout << "Forwarding DENM to LDM" << endl;
+		s_sendmore(*publisher_ldm, topic);
+		s_send(*publisher_ldm, msg_str);
+	}
+}
+	
+void DENM::sendToDccLoop() {
+	//variables
+  	string topic;	
+  	string msg_str;
+  	string text_str;
+
+  	//create DENM
+  	GOOGLE_PROTOBUF_VERIFY_VERSION;
+  	buffers::DENM msg_denm_send; 
+  	msg_denm_send.set_id(2345);
+  	msg_denm_send.set_content("DENM from DENM service");
+	while(1) {
+		//Send DENM to DCC
+		msg_denm_send.SerializeToString(&msg_str);
+		message_t request (msg_str.size());
+		memcpy ((void *) request.data (), msg_str.c_str(), msg_str.size());
+		cout << "Sending DENM to DCC and LDM" << endl;
+		s_sendmore(*publisher_dcc, "DENM");
+		s_send(*publisher_dcc, msg_str);
+		
+		//Send DENM to LDM
+		s_sendmore(*publisher_ldm, "DENM");
+		s_send(*publisher_ldm, msg_str);
+		sleep(3);
+	}
 }
 
 int main () {
   	DENM denm;
-  	denm.loop();
+  	denm.init();
 
   	return EXIT_SUCCESS;
 }
--- denm/src/denm.h
+++ denm/src/denm.h
@@ -1,5 +1,25 @@
+#include <zmq.hpp>
+#include <boost/thread.hpp>
+
 class DENM
 {
 public:
-  void loop();
+	DENM();
+	~DENM();
+	void init();
+	void loop();
+	void receiveFromDccLoop();
+	void sendToDccLoop();
+	void sendToLDM();
+	
+	
+private:
+	zmq::context_t* context;
+	zmq::socket_t* subscriber_dcc;
+	zmq::socket_t* publisher_dcc;
+	zmq::socket_t* publisher_ldm;
+	
+	
+	boost::thread* mSendToDccThread;
+	boost::thread* mReceiveFromDccThread;
 };
