d930146d118f179201dc0465b73430bd2a08352a
Jan Tiemann <jantie@mail.upb.de>
2016-01-14 16:15:58 +0100

    recieve from hardware via IP, untestet for real packets

--- dcc/src/RecieveFromHarwareViaIP.cpp
+++ dcc/src/RecieveFromHarwareViaIP.cpp
@@ -1,18 +1,75 @@
-/*
- * RecieveFromHarwareViaIP.cpp
- *
- *  Created on: Jan 14, 2016
- *      Author: jan
- */
+#define ELPP_THREAD_SAFE
+#define ELPP_NO_DEFAULT_LOG_FILE
+
 
 #include "RecieveFromHarwareViaIP.h"
+#include <string>
+#include <arpa/inet.h>
+#include <sstream>
+
 
 RecieveFromHarwareViaIP::RecieveFromHarwareViaIP() {
-	// TODO Auto-generated constructor stub
 
+	mSenderToDcc = new CommunicationSender("RecieveFromHardware", "4444");
+	mLogger = new LoggingUtility("RecieveFromHardware");
 }
 
 RecieveFromHarwareViaIP::~RecieveFromHarwareViaIP() {
-	// TODO Auto-generated destructor stub
+	mThreadReceive->join();
 }
 
+void RecieveFromHarwareViaIP::init(){
+	/* create a UDP socket */
+
+	if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		mLogger->logDebug("cannot create socket\n");
+		return;
+	}
+
+
+	/* bind the socket to any valid IP address and a specific port */
+
+	memset((char *)&myaddr, 0, sizeof(myaddr));
+	myaddr.sin_family = AF_INET;
+	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	myaddr.sin_port = htons(service_port);
+
+	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
+		mLogger->logDebug("bind failed");
+		return;
+	}
+
+
+
+	mThreadReceive = new boost::thread(&RecieveFromHarwareViaIP::recieve, this);
+
+
+}
+
+void RecieveFromHarwareViaIP::recieve(){
+	int seqno = 0;
+
+	mLogger->logDebug("seqno\tdata\tport\trepetition\n");
+	fflush(stdout);
+	/* now loop, receiving data and printing what we received */
+		while(true) {
+			//fprintf(stderr, "waiting on port %d\n", service_port);
+			recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
+			//printf("received %d bytes\n", recvlen);
+			if (recvlen > 0) {
+				buf[recvlen] = 0;
+				//printf("received message: \"%s\"\n", buf);
+
+				std::ostringstream oss;
+				oss <<  "\n seqno: " << seqno << " buf: "<< buf
+						<< " serservice_port: " << service_port
+						<< " repetition: " << repetition << std::endl;
+				mLogger->logDebug(oss.str());
+				//send to dcc
+				std::string tmp(reinterpret_cast<char*>(buf));
+				mSenderToDcc->sendToHw(tmp);
+			}
+			seqno++;
+		}
+		/* never exits */
+}
--- dcc/src/RecieveFromHarwareViaIP.h
+++ dcc/src/RecieveFromHarwareViaIP.h
@@ -8,10 +8,35 @@
 #ifndef RECIEVEFROMHARWAREVIAIP_H_
 #define RECIEVEFROMHARWAREVIAIP_H_
 
+#include <utility/CommunicationSender.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <boost/thread.hpp>
+#include <zmq.hpp>
+
+#define BUFSIZE 4096
+
 class RecieveFromHarwareViaIP {
 public:
 	RecieveFromHarwareViaIP();
 	virtual ~RecieveFromHarwareViaIP();
+	void init();
+	void recieve();
+
+private:
+	//TODO: rename to fit to convention
+	struct sockaddr_in myaddr;	/* our address */
+	struct sockaddr_in remaddr;	/* remote address */
+	socklen_t addrlen = sizeof(remaddr);		/* length of addresses */
+	int recvlen = 0;			/* # bytes received */
+	int fd =0;					/* our socket */
+	unsigned char buf[BUFSIZE];	/* receive buffer */
+	int service_port = 21234;
+	int repetition = 0;
+
+	CommunicationSender* mSenderToDcc;
+	boost::thread* mThreadReceive;
+	LoggingUtility* mLogger;
 };
 
 #endif /* RECIEVEFROMHARWAREVIAIP_H_ */
