40fb864e37aebe0ceddd5424473561645ec03908
Gurjashan S. Pannu <gspannu@mail.uni-paderborn.de>
2016-02-07 22:48:04 +0100

    Update channel prober

--- channelprober/src/ChannelProber.cpp
+++ channelprober/src/ChannelProber.cpp
@@ -1,6 +1,3 @@
-#define ELPP_THREAD_SAFE
-#define ELPP_NO_DEFAULT_LOG_FILE
-
 #include "ChannelProber.h"
 #include <unistd.h>
 #include <string>
@@ -8,72 +5,74 @@
 #include <linux/nl80211.h>
 #include <net/if.h>
 
-using namespace std;
+#define PRINT_STATS 1
 
-INITIALIZE_EASYLOGGINGPP
+using namespace std;
 
 int ChannelProber::mNl80211Id;
 
-ChannelProber::ChannelProber() {
-	mChannelLoadProbingPeriod = 1000;
-	mTotalWifi = 1;
+ChannelProber::ChannelProber(string ifname) {
+	mIfname = ifname;
 }
 
 ChannelProber::~ChannelProber() {
-	//TODO: check what to do here!
 	mThreadProbe->join();
+	delete mThreadProbe;
+	free(mWifi);
+	nl_socket_modify_cb(mSocket, NL_CB_VALID, NL_CB_CUSTOM,
+			NULL, NULL);
+	nl_socket_free(mSocket);
+	mSocket = NULL;
 }
 
 void ChannelProber::init() {
 	int ret;
-	wifi = (net_interface*) calloc(mTotalWifi, sizeof(net_interface));
-	for (int i = 0; i < mTotalWifi; i++) {
-		wifi[i].ifindex = if_nametoindex("wlan0");
-		cout << "if index: " << wifi[i].ifindex << endl;
-		if (wifi[i].ifindex == 0) {
-			cout << "error getting network interface. errno: " << errno << endl;
-			wifi[i].ifindex = -1;
-			wifi[i].channel = -1;
-		} else {
-			wifi[i].channel = 5890;
-		}
-		wifi[i].load.load = -1;
-		wifi[i].load.total_time_last = 0;
-		wifi[i].load.busy_time_last = 0;
+	mWifi = (netinterface*) calloc(1, sizeof(netinterface));
+	mWifi->ifindex = if_nametoindex(mIfname.c_str());
+	if (mWifi->ifindex == 0) {
+		cout << "ChannelProber: error getting network interface. errno: " << errno << endl;
+		mWifi->ifindex = -1;
+		mWifi->channel = -1;
 	}
+	mWifi->load.load = -1;
+	mWifi->load.totalTimeLast = 0;
+	mWifi->load.busyTimeLast = 0;
 
 	// initialize socket now
 	mSocket = nl_socket_alloc();
 	if (mSocket == NULL) {
-		cout << "socket_alloc error" << endl;
+		cout << "ChannelProber: nl_socket_alloc error" << endl;
 		return;
 	}
+
 	// disable sequence number checking. We want to receive notifications.
 	nl_socket_disable_seq_check(mSocket);
 
 	ret = nl_socket_modify_cb(mSocket, NL_CB_VALID, NL_CB_CUSTOM,
 			ChannelProber::receivedNetlinkMsg, this);
-	cout << "NL Socket Callback ret: " << ret << endl;
-
+	if(ret != 0) {
+		cout << "ChannelProber: error when setting callback : " << ret << endl;
+	}
 	// connect socket. Protocol: generic netlink
 	ret = genl_connect(mSocket);
 	if (ret != 0) {
-		cout << "Connection to Generic Netlink Socket failed! err: "
+		cout << "ChannelProber: Connection to Generic Netlink Socket failed! err: "
 				<< nl_geterror(ret) << endl;
 		return;
 	}
+
 	// resolve mNl80211Id
 	mNl80211Id = genl_ctrl_resolve(mSocket, "nl80211");
 	if (mNl80211Id < 0) {
-		cout << "error code getting nl80211 id: " << nl_geterror(mNl80211Id)
+		cout << "ChannelProber: error code getting nl80211 id: " << nl_geterror(mNl80211Id)
 				<< endl;
 		return;
 	}
+
 	mThreadProbe = new boost::thread(&ChannelProber::probe, this);
 }
 
 int ChannelProber::receivedNetlinkMsg(nl_msg *msg, void *arg) {
-	// cout << "Received netlink message!" << endl;
 	ChannelProber* cp = (ChannelProber*) arg;
 
 	struct nlattr *tb[NL80211_ATTR_MAX + 1];
@@ -89,134 +88,159 @@ int ChannelProber::receivedNetlinkMsg(nl_msg *msg, void *arg) {
 			genlmsg_attrlen(gnlh, 0), NULL);
 
 	if_indextoname(nla_get_u32(tb[NL80211_ATTR_IFINDEX]), dev);
-	// printf("Survey data from %s\n", dev);
 
 	if (!tb[NL80211_ATTR_SURVEY_INFO]) {
-		fprintf(stderr, "survey data missing!\n");
+		cout << "ChannelProber: survey data missing!" << endl;
 		return NL_SKIP;
 	}
 	static struct nla_policy survey_policy[NL80211_SURVEY_INFO_MAX + 1] = { };
 
 	if (nla_parse_nested(sinfo, NL80211_SURVEY_INFO_MAX,
 			tb[NL80211_ATTR_SURVEY_INFO], survey_policy)) {
-		fprintf(stderr, "failed to parse nested attributes!\n");
+		cout << "ChannelProber: failed to parse nested attributes!" << endl;
 		return NL_SKIP;
 	}
 
 	// If this info is not about the channel in use, then skip
 	if (!sinfo[NL80211_SURVEY_INFO_IN_USE]) {
-//		cout << "channel not in use. so returning" << endl;
 		return NL_SKIP;
 	}
+
+	// Current channel in use
 	if (sinfo[NL80211_SURVEY_INFO_FREQUENCY]) {
 		channel = nla_get_u32(sinfo[NL80211_SURVEY_INFO_FREQUENCY]);
-		cout << "\tfrequency:\t\t\t" << channel << " MHz" << endl;
-	}
-	if (sinfo[NL80211_SURVEY_INFO_NOISE]) {
-		noise = (int8_t) nla_get_u8(sinfo[NL80211_SURVEY_INFO_NOISE]);
-//		cout<<"\tnoise:\t\t\t\t"<< noise <<" dBm"<<endl;
-	}
-	if (sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME]) {
-		total_time = nla_get_u64(sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME]);
-		cout << "\tchannel active time:\t\t" << total_time << " ms" << endl;
-	}
-	if (sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME_BUSY]) {
-		busy_time = nla_get_u64(sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME_BUSY]);
-		cout << "\tchannel busy time:\t\t" << busy_time << " ms" << endl;
-	}
-	if (sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME_EXT_BUSY]) {
-//		printf("\textension channel busy time:\t%llu ms\n",
-//				(unsigned long long) nla_get_u64(
-//						sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME_EXT_BUSY]));
-	}
-	if (sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME_RX]) {
-		cout << "\tchannel receive time:\t\t"
-				<< nla_get_u64(sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME_RX])
-				<< " ms" << endl;
-	}
-	if (sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME_TX]) {
-		cout << "\tchannel transmit time:\t\t"
-				<< nla_get_u64(sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME_TX])
-				<< " ms" << endl;
+		if(PRINT_STATS) {
+			cout << "\tfrequency:\t\t\t" << channel << " MHz" << endl;
+		}
 	}
 
-	// Update stats
-	uint64_t busy_time_diff = busy_time - cp->wifi[0].load.busy_time_last;
-	uint64_t total_time_diff = total_time - cp->wifi[0].load.total_time_last;
+	// Noise
+	if (sinfo[NL80211_SURVEY_INFO_NOISE]) {
+		noise = (int8_t) nla_get_u8(sinfo[NL80211_SURVEY_INFO_NOISE]);
+		// cout<<"\tnoise:\t\t\t\t"<< noise <<" dBm"<<endl;
+	}
+
+	// Total active time
+	if (sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME]) {
+		total_time = nla_get_u64(sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME]);
+		if(PRINT_STATS) {
+			cout << "\tchannel active time:\t\t" << total_time << " ms" << endl;
+		}
+	}
+
+	// Total busy time
+	if (sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME_BUSY]) {
+		busy_time = nla_get_u64(sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME_BUSY]);
+		if(PRINT_STATS) {
+			cout << "\tchannel busy time:\t\t" << busy_time << " ms" << endl;
+		}
+	}
+
+	// Do we need info about extension channel?
+	if (sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME_EXT_BUSY]) {
+		// cout << "\textension channel busy time:\t"
+		//		<< nla_get_u64(sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME_EXT_BUSY])
+		//		<< " ms" << endl;
+	}
+
+	// Total receiving time
+	if (sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME_RX]) {
+		if(PRINT_STATS) {
+			cout << "\tchannel receive time:\t\t"
+				<< nla_get_u64(sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME_RX])
+				<< " ms" << endl;
+		}
+	}
+
+	// Total transmitting time
+	if (sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME_TX]) {
+		if(PRINT_STATS) {
+			cout << "\tchannel transmit time:\t\t"
+				<< nla_get_u64(sinfo[NL80211_SURVEY_INFO_CHANNEL_TIME_TX])
+				<< " ms" << endl;
+		}
+	}
+
+	// Update statistics
+	uint64_t busy_time_diff = busy_time - cp->mWifi->load.busyTimeLast;
+	uint64_t total_time_diff = total_time - cp->mWifi->load.totalTimeLast;
 	double load = ((100 * busy_time_diff) / total_time_diff) / 100.0;
-	cp->wifi[0].load.mutex_channelLoad.lock();
-	cp->wifi[0].load.load = load;
-	cp->wifi[0].load.total_time_last = total_time;
-	cp->wifi[0].load.busy_time_last = busy_time;
-	cp->wifi[0].load.noise = noise;
-	cp->wifi[0].load.mutex_channelLoad.unlock();
-	cout << "time since last check: " << total_time_diff << endl;
-	cout << "busy time since last check: " << busy_time_diff << endl;
-	cout << "Calculated Load: " << load << endl;
-	cout << "-----------------------" << endl;
+	cp->mWifi->load.mutexChannelLoad.lock();
+	cp->mWifi->load.load = load;
+	cp->mWifi->load.totalTimeLast = total_time;
+	cp->mWifi->load.busyTimeLast = busy_time;
+	cp->mWifi->load.noise = noise;
+	cp->mWifi->load.mutexChannelLoad.unlock();
+	if(PRINT_STATS) {
+		cout << "time since last check: " << total_time_diff << endl;
+		cout << "busy time since last check: " << busy_time_diff << endl;
+		cout << "Calculated Load: " << load << endl;
+		cout << "-----------------------" << endl;
+	}
 	return NL_SKIP;
 }
 
 void ChannelProber::probe() {
-	// sleep(5);
-	cout << "started probing every 1 sec" << endl;
+	cout << "Started probing " << endl;
 	while (true) {
-		send_nl80211(NL80211_CMD_GET_SURVEY, &wifi[0].ifindex,
-				sizeof(wifi[0].ifindex), NL80211_ATTR_IFINDEX, NL_AUTO_SEQ,
+		sendNl80211(NL80211_CMD_GET_SURVEY, &mWifi->ifindex,
+				sizeof(mWifi->ifindex), NL80211_ATTR_IFINDEX, NL_AUTO_SEQ,
 				NLM_F_DUMP);
 		nl_recvmsgs_default(mSocket);
 		sleep(1);
 	}
 }
 
-int ChannelProber::send_nl80211(uint8_t msgCmd, void *payload,
-		unsigned int pLength, int payloadType, unsigned int seq, int flags) {
-	return send(msgCmd, payload, pLength, payloadType, seq, mNl80211Id, flags,
+int ChannelProber::sendNl80211(uint8_t msgCmd, void *payload,
+		unsigned int length, int payloadType, unsigned int seq, int flags) {
+	return send(msgCmd, payload, length, payloadType, seq, mNl80211Id, flags,
 			0x01);
 }
 
-int ChannelProber::send(uint8_t msgCmd, void *payload, unsigned int pLength,
-		int attrType, unsigned int seq, int protocol_id, int flags,
-		uint8_t protocol_version) {
+int ChannelProber::send(uint8_t msgCmd, void *payload, unsigned int length,
+		int attrType, unsigned int seq, int protocolId, int flags,
+		uint8_t protocolVersion) {
 	int ret = 0;
 
 	// create a new Netlink message
 	nl_msg *msg = nlmsg_alloc();
 
 	// create message header
-	if (genlmsg_put(msg, 0, seq, protocol_id, 0, flags, msgCmd,
-			protocol_version) == NULL) {
-		cout
-				<< "ChannelProber: Error creating generic netlink header for message."
-				<< endl;
+	if (genlmsg_put(msg, 0, seq, protocolId, 0, flags, msgCmd,
+			protocolVersion) == NULL) {
+		cout << "ChannelProber: Error creating generic netlink header for message." << endl;
 		return -1;
 	}
 
 	// add message attributes (=payload)
-	if (pLength > 0) {
-		ret = nla_put(msg, attrType, pLength, payload);
+	if (length > 0) {
+		ret = nla_put(msg, attrType, length, payload);
 		if (ret < 0) {
-			printf("ChannelProber: Error message add attribute: %s\n",
-					nl_geterror(ret));
+			cout << "ChannelProber: Error message add attribute: " << nl_geterror(ret) << endl;
 			return ret;
 		}
 	}
 
 	ret = nl_send_auto(mSocket, msg);
 	if (ret < 0) {
-		printf("ChannelProber: Error sending message. Error code: %s\n",
-				nl_geterror(ret));
-	} else {
-		printf("ChannelProber::send: %d bytes sent\n", ret);
+		cout << "ChannelProber: Error sending message. Error code: " << nl_geterror(ret) << endl;
 	}
 	nlmsg_free(msg);
 
 	return ret;
 }
 
+double ChannelProber::getChannelLoad() {
+	float load;
+	mWifi->load.mutexChannelLoad.lock();
+	load = mWifi->load.load;
+	mWifi->load.mutexChannelLoad.unlock();
+	return (double) load;
+}
+
 int main() {
-	ChannelProber prober;
-	prober.init();
+	ChannelProber cp("wlan0");
+	cp.init();
 
 	return 0;
 }
--- channelprober/src/ChannelProber.h
+++ channelprober/src/ChannelProber.h
@@ -3,47 +3,45 @@
 
 #include <boost/thread/mutex.hpp>
 #include <boost/thread.hpp>
-#include <utility/LoggingUtility.h>
 #include <netlink/netlink-kernel.h>
 #include <netlink/socket.h>
 #include <netlink/genl/genl.h>
 #include <netlink/genl/ctrl.h>
 #include <netlink/genl/mngt.h>
 
+using namespace std;
+
 class ChannelProber {
 public:
-	ChannelProber();
+	ChannelProber(string ifname);
 	virtual ~ChannelProber();
 
 	void init();
 	void probe();
-	int send_nl80211(uint8_t msgCmd, void *payload, unsigned int pLength, int payloadType, unsigned int seq, int flags = 0);
-	int send(uint8_t msgCmd, void *payload, unsigned int pLength, int attrType, unsigned int seq, int protocol_id, int flags = 0, uint8_t protocol_version = 0x01);
-
-	// Callback functions for netlink. Not to be used by user!
+	int sendNl80211(uint8_t msgCmd, void *payload, unsigned int length, int payloadType, unsigned int seq, int flags = 0);
+	int send(uint8_t msgCmd, void *payload, unsigned int length, int attrType, unsigned int seq, int protocolId, int flags = 0, uint8_t protocolVersion = 0x01);
+	double getChannelLoad();
 	static int receivedNetlinkMsg(nl_msg *msg, void *arg);
 
 	struct channelload {
-		boost::mutex mutex_channelLoad;
+		boost::mutex mutexChannelLoad;
 		uint8_t noise;
-		uint64_t total_time_last;
-		uint64_t busy_time_last;
+		uint64_t totalTimeLast;
+		uint64_t busyTimeLast;
 		double load;
 	};
-	struct net_interface {
+	struct netinterface {
 		unsigned int ifindex;
 		unsigned int channel;
 		channelload load;
 	};
 
 	nl_sock *mSocket;
-	int socket_state;
 	static int mNl80211Id;
 	boost::thread* mThreadProbe;
 
-	net_interface *wifi;
-	int mTotalWifi;
-	unsigned int mChannelLoadProbingPeriod;
+	netinterface *mWifi;
+	string mIfname;
 };
 
 #endif
