e4932fcfd00786d67ed147f300f11f110719b592
Stefan Schneider <stschn@mail.upb.de>
2016-05-27 17:21:55 +0200

    Added more SQLite functionality and print functions to LDM. Adjusted db

--- ldm/src/ldm.cpp
+++ ldm/src/ldm.cpp
@@ -36,10 +36,31 @@ LDM::~LDM() {
 	sqlite3_close(mDb);
 }
 
-//SQLite functions
+void LDM::init() {
+	mThreadReceiveFromCa = new boost::thread(&LDM::receiveFromCa, this);
+	mThreadReceiveFromDen = new boost::thread(&LDM::receiveFromDen, this);
 
-//TODO: select last_insert_rowid();
-//TODO: improve modularization, separation of SQL from functionality
+	//test GPS
+//	list<gpsPackage::GPS> gpsList = gpsSelect("");
+//	for (list<gpsPackage::GPS>::iterator it = gpsList.begin(); it != gpsList.end(); ++it) {
+//		printGps(*it);
+//	}
+
+	//test OBD2
+//	list<obd2Package::OBD2> obd2List = obd2Select("");
+//	for (list<obd2Package::OBD2>::iterator it = obd2List.begin(); it != obd2List.end(); ++it) {
+//		printObd2(*it);
+//	}
+
+	//test CAM
+//	list<camPackage::CAM> camList = camSelect("");
+//	for (list<camPackage::CAM>::iterator it = camList.begin(); it != camList.end(); ++it) {
+//		printCam(*it);
+//	}
+}
+
+
+//////////SQLite functions
 
 //executes specified insert
 void LDM::insert(string sqlCommand) {
@@ -52,18 +73,19 @@ void LDM::insert(string sqlCommand) {
 	}
 }
 
-//executes specified select on GPS table and returns result rows as list of GPS data	//TODO: add for cam, denm, obd2
-list<gpsPackage::GPS> LDM::gpsSelect(string sqlCommand) {
+//executes SELECT with specified condition (eg. WHERE) on GPS table and returns result rows as list of GPS data
+list<gpsPackage::GPS> LDM::gpsSelect(string condition) {
 	list<gpsPackage::GPS> result;
 	result.clear();
 	sqlite3_stmt *stmt;
+	string sqlCommand = "SELECT * from GPS " + condition;
 	char* errmsg = 0;
 
 	if (sqlite3_prepare_v2(mDb, sqlCommand.c_str(), -1, &stmt, NULL) == SQLITE_OK) {
 		while (sqlite3_step(stmt) == SQLITE_ROW) {		//iterate over result rows
 			gpsPackage::GPS gps;
 
-			//set attributes
+			//set attributes retrieved from result columns
 			gps.set_latitude(sqlite3_column_double(stmt, 1));
 			gps.set_longitude(sqlite3_column_double(stmt, 2));
 			gps.set_altitude(sqlite3_column_double(stmt, 3));
@@ -74,11 +96,82 @@ list<gpsPackage::GPS> LDM::gpsSelect(string sqlCommand) {
 			gps.set_satellites(sqlite3_column_int(stmt, 8));
 
 			result.push_back(gps);	//add to result
+		}
+		sqlite3_finalize(stmt);
+	}
+	else {
+		string error(errmsg);
+		mLogger->logError("SQL error: " + error);
+		sqlite3_free(errmsg);
+	}
 
-			//print for testing
-			string textMessage;
-			google::protobuf::TextFormat::PrintToString(gps, &textMessage);
-			cout << textMessage << endl;
+	return result;
+}
+
+//executes specified SELECT with specified condition (eg. WHERE) on OBD2 table and returns result rows as list of OBD2 data
+list<obd2Package::OBD2> LDM::obd2Select(string condition) {
+	list<obd2Package::OBD2> result;
+	result.clear();
+	sqlite3_stmt *stmt;
+	string sqlCommand = "SELECT * from OBD2 " + condition;
+	char* errmsg = 0;
+
+	if (sqlite3_prepare_v2(mDb, sqlCommand.c_str(), -1, &stmt, NULL) == SQLITE_OK) {
+		while (sqlite3_step(stmt) == SQLITE_ROW) {		//iterate over result rows
+			obd2Package::OBD2 obd2;
+
+			//set attributes retrieved from result columns
+			obd2.set_time(sqlite3_column_int64(stmt, 1));
+			obd2.set_speed(sqlite3_column_double(stmt, 2));
+			obd2.set_rpm(sqlite3_column_int(stmt, 3));
+
+			result.push_back(obd2);	//add to result
+		}
+		sqlite3_finalize(stmt);
+	}
+	else {
+		string error(errmsg);
+		mLogger->logError("SQL error: " + error);
+		sqlite3_free(errmsg);
+	}
+
+	return result;
+}
+
+//executes specified SELECT with specified condition (eg. WHERE) on CAM table and returns result rows as list of CAMs
+list<camPackage::CAM> LDM::camSelect(string condition) {
+	list<camPackage::CAM> result;
+	result.clear();
+	sqlite3_stmt *stmt;
+	string sqlCommand = "SELECT * from CAM " + condition;
+	char* errmsg = 0;
+
+	if (sqlite3_prepare_v2(mDb, sqlCommand.c_str(), -1, &stmt, NULL) == SQLITE_OK) {
+		while (sqlite3_step(stmt) == SQLITE_ROW) {		//iterate over result rows
+			camPackage::CAM cam;
+
+			//set attributes retrieved from result columns
+			cam.set_id(sqlite3_column_int(stmt, 1));
+			cam.set_content(string(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2))));
+			cam.set_createtime(sqlite3_column_int64(stmt, 3));
+
+			//add GPS if available
+			int64_t gpsRowId = sqlite3_column_int64(stmt, 4);
+			if (gpsRowId > 0) {
+				list<gpsPackage::GPS> gpsList = gpsSelect("WHERE key=" + to_string(gpsRowId));
+				gpsPackage::GPS* gps = new gpsPackage::GPS(gpsList.front());
+				cam.set_allocated_gps(gps);
+			}
+
+			//add OBD2 if available
+			int64_t obd2RowId = sqlite3_column_int64(stmt, 5);
+			if (obd2RowId > 0) {
+				list<obd2Package::OBD2> obd2List = obd2Select("WHERE key=" + to_string(obd2RowId));
+				obd2Package::OBD2* obd2 = new obd2Package::OBD2(obd2List.front());
+				cam.set_allocated_obd2(obd2);
+			}
+
+			result.push_back(cam);	//add to result
 		}
 		sqlite3_finalize(stmt);
 	}
@@ -94,6 +187,8 @@ list<gpsPackage::GPS> LDM::gpsSelect(string sqlCommand) {
 //inserts CAM into DB
 void LDM::insertCam(camPackage::CAM cam) {
 	stringstream sSql;
+	int64_t gpsRowId = -1;
+	int64_t obd2RowId = -1;
 
 	//insert GPS if available
 	if (cam.has_gps()) {
@@ -101,6 +196,7 @@ void LDM::insertCam(camPackage::CAM cam) {
 		insert(sSql.str());
 		sSql.str("");
 		sSql.clear();
+		gpsRowId = sqlite3_last_insert_rowid(mDb);
 	}
 
 	//insert OBD2 if available
@@ -109,23 +205,72 @@ void LDM::insertCam(camPackage::CAM cam) {
 		insert(sSql.str());
 		sSql.str("");
 		sSql.clear();
+		obd2RowId = sqlite3_last_insert_rowid(mDb);
 	}
 
-	//insert CAM
-	sSql << "INSERT INTO CAM (id, content, createTime) VALUES (" << cam.id() << ", '" << cam.content() << "', " << cam.createtime() << " );";
+	//insert CAM with foreign keys to reference GPS, OBD2
+	if (gpsRowId > 0 && obd2RowId > 0) {
+		sSql << "INSERT INTO CAM (id, content, createTime, gps, obd2) VALUES (" << cam.id() << ", '" << cam.content() << "', " << cam.createtime() << ", " << gpsRowId << ", " << obd2RowId << " );";
+	}
+	else if (gpsRowId > 0) {
+		sSql << "INSERT INTO CAM (id, content, createTime, gps) VALUES (" << cam.id() << ", '" << cam.content() << "', " << cam.createtime() << ", " << gpsRowId << " );";
+	}
+	else if (obd2RowId > 0) {
+		sSql << "INSERT INTO CAM (id, content, createTime, obd2) VALUES (" << cam.id() << ", '" << cam.content() << "', " << cam.createtime() << ", " << obd2RowId << " );";
+	}
+	else {
+		sSql << "INSERT INTO CAM (id, content, createTime) VALUES (" << cam.id() << ", '" << cam.content() << "', " << cam.createtime() << " );";
+	}
 	insert(sSql.str());
-	sSql.str("");
-	sSql.clear();
 }
 
-//LDM functions
 
-void LDM::init() {
-	mThreadReceiveFromCa = new boost::thread(&LDM::receiveFromCa, this);
-	mThreadReceiveFromDen = new boost::thread(&LDM::receiveFromDen, this);
+//////////log/print function
+//converts ns since epoch into human readable format (HH:MM:SS,ms)
+string LDM::readableTime(int64_t nanoTime) {
+	char buffer[9];
+	int64_t milliTime = nanoTime / (1*1000*1000);		//ns to ms (since epoch)
+	time_t epochTime = milliTime / 1000;				//ms to s (since epoch)
+	struct tm* timeinfo = localtime(&epochTime);
+	strftime(buffer, 9, "%T", timeinfo);				//buffer contains time HH:MM:SS
+	int ms = milliTime % epochTime;						//just the ms
 
-//	gpsSelect("SELECT * from GPS");	//for testing
+	stringstream time;								//convert to string
+	time << buffer << "," << ms;
+
+	return time.str();
 }
+
+void LDM::printGps(gpsPackage::GPS gps) {
+	stringstream stream;
+
+	stream << "GPS - " << readableTime(gps.time()) << ", lat: " << gps.latitude() << ", long: " << gps.longitude() << ", alt: " << gps.altitude();
+	//TODO: include epx, epy, ...?
+
+	mLogger->logInfo(stream.str());
+}
+
+void LDM::printObd2(obd2Package::OBD2 obd2) {
+	stringstream stream;
+	stream << "OBD2 - " << readableTime(obd2.time()) << ", speed: " << obd2.speed() << ", rpm: " << obd2.rpm();
+	mLogger->logInfo(stream.str());
+}
+
+void LDM::printCam(camPackage::CAM cam) {
+	stringstream stream;
+	stream << "CAM - " << readableTime(cam.createtime()) << ", id: " << cam.id() << ", content: " << cam.content();
+	if (cam.has_gps()) {
+
+		stream << "\n\tGPS - " << readableTime(cam.gps().time()) << ", lat: " << cam.gps().latitude() << ", long: " << cam.gps().longitude() << ", alt: " << cam.gps().altitude();
+	}
+	if (cam.has_obd2()) {
+		stream << "\n\tOBD2 - " << readableTime(cam.obd2().time()) << ", speed: " << cam.obd2().speed() << ", rpm: " << cam.obd2().rpm();
+	}
+	mLogger->logInfo(stream.str());
+}
+
+
+//////////LDM functions
 
 void LDM::receiveFromCa() {
 	string serializedCam;	//serialized CAM
@@ -160,6 +305,8 @@ void LDM::receiveFromDen() {
 		denm.ParseFromString(serializedDenm);
 		google::protobuf::TextFormat::PrintToString(denm, &textMessage);
 		mLogger->logInfo("received DENM:\n" + textMessage);
+
+//		insertDenm(denm);
 	}
 }
 
--- ldm/src/ldm.h
+++ ldm/src/ldm.h
@@ -8,19 +8,31 @@
 #include <buffers/build/cam.pb.h>
 #include <buffers/build/denm.pb.h>
 #include <buffers/build/gps.pb.h>
+#include <buffers/build/obd2.pb.h>
 #include <google/protobuf/text_format.h>
 #include <string>
+#include <ctime>
 
 class LDM {
 public:
 	LDM();
 	~LDM();
+	void init();
 
 	void insert(string sqlCommand);
-	list<gpsPackage::GPS> gpsSelect(string sqlCommand);
+	list<gpsPackage::GPS> gpsSelect(string condition);
+	list<obd2Package::OBD2> obd2Select(string condition);
+	list<camPackage::CAM> camSelect(string condition);
+	list<denmPackage::DENM> denmSelect(string condition);	//TODO: implement
 	void insertCam(camPackage::CAM cam);
+	void insertDenm(denmPackage::DENM denm);	//TODO: implement
 
-	void init();
+	string readableTime(int64_t nanoTime);
+	void printGps(gpsPackage::GPS gps);
+	void printObd2(obd2Package::OBD2 obd2);
+	void printCam(camPackage::CAM cam);
+	void printDenm(denmPackage::DENM denm);		//TODO: implement
+
 	void receiveFromCa();
 	void receiveFromDen();
 
@@ -33,7 +45,7 @@ private:
 
 	LoggingUtility* mLogger;
 
-	sqlite3 *mDb;
+	sqlite3* mDb;
 };
 
 #endif
