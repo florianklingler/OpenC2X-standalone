eb7fd897fa22dc729df0c35fa1a4b5a7ad014cff
Sven Laux <slaux@mail.uni-paderborn.de>
2016-06-01 15:17:14 +0200

    added Frontend and WebApplication

---
+++ ldmFrontend/scrips/main.js
@@ -0,0 +1,26 @@
+
+
+function queryLdmBackend(){
+	$.post("http://localhost:1188/add_json",JSON.stringify({a:2,b:3}),function(data,status,xhr){
+		console.log("data: "+data);
+		console.log("status: "+status);
+	});
+
+}
+
+queryLdmBackend();
+
+$(document).ready(function(){
+	//init map
+	var map = L.map('mapContainer');
+
+	// create the tile layer with correct attribution
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	//marbel chache file:///home/jonh/.local/share/marble/maps/earth/openstreetmap/{z}/{x}/{y}.png
+	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 12, maxZoom: 20, attribution: osmAttrib});		
+
+	// start the map in South-East England
+	map.setView(new L.LatLng(51.7315, 8.739),17);
+	map.addLayer(osm);
+});
---
+++ ldmWebApplication/src/Server.h
@@ -0,0 +1,18 @@
+/*
+ * Server.h
+ *
+ *  Created on: May 25, 2016
+ *      Author: root
+ */
+
+#ifndef SERVER_H_
+#define SERVER_H_
+
+class Server {
+public:
+	Server();
+	virtual ~Server();
+	void run();
+};
+
+#endif /* SERVER_H_ */
