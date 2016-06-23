4694bdabf1e257cd8231af2c4c9571434a0e8a0b
Jan Tiemann <jantie@mail.upb.de>
2016-06-23 17:32:31 +0200

    run all tmux uses now tiled layout

--- scripts/runAllTmuxSSH.sh
+++ scripts/runAllTmuxSSH.sh
@@ -13,11 +13,7 @@ tmux new-window -t $SESSION:1 -n 'App'
 tmux set-option -g mouse
 
 
-
-tmux split-window -h
-tmux select-pane -t 0
-
-for APP in ldm ldmWebApplication cam 
+for APP in ldm ldmWebApplication cam gps obd2 denm
 do
     echo "Starting $APP"
     # C-m semms to equal to pressing enter -> line is executed
@@ -27,22 +23,8 @@ do
     tmux send-keys "cd ../$APP/Debug" C-m
     tmux send-keys "./$APP" C-m
     tmux split-window -v
+    tmux select-layout tiled
 done 
-
-tmux kill-pane
-tmux select-pane -t 3
-
-for APP in gps obd2 denm
-do
-    echo "Starting $APP"
-    tmux send-keys "ssh $BOX" C-m
-    sleep $SSH_WAIT_TIME
-    tmux send-keys "cd pg2015w/scripts" C-m
-    tmux send-keys "cd ../$APP/Debug" C-m
-    tmux send-keys "./$APP" C-m
-    tmux split-window -v
-done 
-
 
 echo "Starting dcc"
 tmux send-keys "ssh $BOX" C-m
