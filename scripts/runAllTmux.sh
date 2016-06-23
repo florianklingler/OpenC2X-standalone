c3e53ca0670df5fb18279fb7be23ae7f8c9fca59
Jan Tiemann <jantie@mail.upb.de>
2016-06-23 16:51:50 +0200

    addition to last commit

--- scripts/runAllTmux.sh
+++ scripts/runAllTmux.sh
@@ -3,34 +3,34 @@ SESSION=ETSI
 
 tmux -2 new-session -d -s $SESSION
 
-tmux -2 attach-session -t $SESSION
 
-# tmux set-option -g mouse
-# 
-# tmux new-window -t $SESSION:1 -n 'App'
-# 
-# 
-# tmux split-window -h
-# tmux select-pane -t 0
-# 
-# for APP in ldm ldmWebApplication cam 
-# do
-#     tmux send-keys "cd ../$APP/Debug" C-m
-#     tmux send-keys "./$APP" C-m
-#     tmux split-window -v
-# done 
-# 
-# tmux kill-pane
-# tmux select-pane -t 3
-# 
-# for APP in gps obd2 denm
-# do
-#     tmux send-keys "cd ../$APP/Debug" C-m
-#     tmux send-keys "./$APP" C-m
-#     tmux split-window -v
-# done 
-# 
-# tmux send-keys "cd ../dcc/Debug" C-m
-# tmux send-keys "sudo ./dcc" C-m
-# 
+tmux set-option -g mouse
+
+tmux new-window -t $SESSION:1 -n 'App'
+
+
+tmux split-window -h
+tmux select-pane -t 0
+
+for APP in ldm ldmWebApplication cam 
+do
+    tmux send-keys "cd ../$APP/Debug" C-m
+    tmux send-keys "./$APP" C-m
+    tmux split-window -v
+done 
+
+tmux kill-pane
+tmux select-pane -t 3
+
+for APP in gps obd2 denm
+do
+    tmux send-keys "cd ../$APP/Debug" C-m
+    tmux send-keys "./$APP" C-m
+    tmux split-window -v
+done 
+
+tmux send-keys "cd ../dcc/Debug" C-m
+tmux send-keys "sudo ./dcc" C-m
+
+tmux -2 attach-session -t $SESSION
 
