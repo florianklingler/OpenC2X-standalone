2623927beab3f43eb1c0915b1888c0c0923759f9
Jan Tiemann <jantie@mail.upb.de>
2016-06-23 18:47:39 +0200

    put explanation at the beginning of runAll scripts; runAllTmuxSSH now also starts the webpage

--- scripts/runAllTmux.sh
+++ scripts/runAllTmux.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 SESSION=ETSI
 
+#Runs all application locally in one terminal window tiled with tmux.
+#If the connection to the tmux session is lost, reconnect via executing "tmux attach".
+#To kill all applications execute "tmux kill-session" on the device
+
+#navigating:
+#press "Ctrl + b" and then a arrow key to change the focus to a panel in the corresponding direction
+#resizing:
+#press "Ctrl + b" then "Alt + Arrow key" to resize a panel in that direction
+#new panel:
+#press "Ctrl + b" then " " " or rather "Shift + 2" to  split a panel
+
+
 tmux -2 new-session -d -s $SESSION
 
 
--- scripts/runAllTmuxSSH.sh
+++ scripts/runAllTmuxSSH.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 SESSION=ETSI
 BOX=gericom1
-SSH_WAIT_TIME=1
+SSH_WAIT_TIME=0.2s # not sure whether needed but if running the app fails after connecting, try increasing this value
+
+#Opens for every programm a seperate ssh connection to $BOX and runs it there.
+#All ssh connections are displayed in seperate tmux panels.
+#Panels may be resized by mouse dragging. Also the focus can be changed by mouse clicking.
+#The applications stop running if the ssh connection is closed/lost.
+#To close all panels and with them the ssh connections execute "tmux kill-session" in any terminal
 
 echo "Starting"
 
@@ -26,6 +32,12 @@ do
     tmux select-layout tiled
 done 
 
+echo "Stating SSH Tunnel"
+tmux send-keys "ssh -L 1188:localhost:1188 $BOX -N" C-m
+
+tmux split-window -v
+tmux select-layout tiled
+
 echo "Starting dcc"
 tmux send-keys "ssh $BOX" C-m
 sleep $SSH_WAIT_TIME
@@ -33,5 +45,7 @@ tmux send-keys "cd pg2015w/scripts" C-m
 tmux send-keys "cd ../dcc/Debug" C-m
 tmux send-keys "sudo ./dcc" C-m
 
+
+firefox "./../ldmWebSite/index.html"&
 
 tmux -2 attach-session -t $SESSION
