5f2a58d90e7024f3e2b0cee4bc6e7b58ab3e6282
Stefan Schneider <stschn@mail.upb.de>
2016-06-30 20:07:45 +0200

    Added doxygen comments for CaService, adjusted privacy of functions.

--- cam/src/caservice.h
+++ cam/src/caservice.h
@@ -37,19 +37,84 @@ public:
 	CaService(CaServiceConfig &config);
 	~CaService();
 
-	void receive();
-	void logDelay(std::string byteMessage);
-	void sendCamInfo(std::string triggerReason, double delta);
+	/** Sends a new CAM to LDM and DCC.
+	 *
+	 */
 	void send();
-	void triggerCam(const boost::system::error_code &ec);
-	camPackage::CAM generateCam();
-	dataPackage::DATA generateData(camPackage::CAM cam);
-	void receiveGpsData();
-	void receiveObd2Data();
+
+	/** Calculates the heading towards North based on the two specified coordinates.
+	 *
+	 * @param lat1 Latitude of coordinate 1.
+	 * @param lon1 Longitude of coordinate 1.
+	 * @param lat2 Latitude of coordinate 2.
+	 * @param lon2 Longitude of coordinate 2.
+	 * @return The heading in degrees.
+	 */
 	double getHeading(double lat1, double lon1, double lat2, double lon2);
+
+	/** Calculates the distance between the two specified coordinates
+	 *
+	 * @param lat1 Latitude of coordinate 1.
+	 * @param lon1 Longitude of coordinate 1.
+	 * @param lat2 Latitude of coordinate 2.
+	 * @param lon2 Longitude of coordinate 2.
+	 * @return The distance in meters.
+	 */
 	double getDistance(double lat1, double lon1, double lat2, double lon2);
 
 private:
+	/** Receives incoming CAMs from DCC and forwards them to LDM.
+	 * Receives serialized DATA packages from DCC, deserializes it, and forwards the contained serialized CAM to LDM.
+	 */
+	void receive();
+
+	/** Logs the delay of the specified (serialized) CAM between its creation time and the current time.
+	 *
+	 * @param serializedCam
+	 */
+	void logDelay(std::string serializedCam);
+
+	/** Sends information about why a CAM was triggered to LDM.
+	 * Sends serialized camInfo to LDM, including the current time and the specified reason for triggering.
+	 * @param triggerReason Difference in time, heading, position, or speed.
+	 * @param delta Specifies how big the difference in time, heading, position, or speed is.
+	 */
+	void sendCamInfo(std::string triggerReason, double delta);
+
+	/** Periodically checks the rules for triggering a new CAM.
+	 * Checks every 100ms whether the difference in time, heading, position, or speed requires the triggering of a new CAM. Reasons for triggering are:
+	 * The time difference since the last triggered CAM is at least one second.
+	 * The heading since the last CAM changed by more than four degrees.
+	 * The position since the last CAM changed by more than five meters.
+	 * The speed since the last CAM changed by more than 1m/s.
+	 * @param ec Boost error code
+	 */
+	void triggerCam(const boost::system::error_code &ec);
+
+	/** Generates a new CAM.
+	 * The new CAM includes the MAC address as stationId, an increasing but not unique ID, a current time stamp, and the latest GPS and OBD2 data if it is not too old (as configured).
+	 * @return The newly generated CAM.
+	 */
+	camPackage::CAM generateCam();
+
+	/** Generates a new data package that includes the specified CAM.
+	 * The specified CAM is serialized and included in the new data package.
+	 * Additionally, the ID, type, priority, creation time and life time of the specified CAM are included.
+	 * @param cam The CAM to be included in the data package.
+	 * @return The newly generated data package.
+	 */
+	dataPackage::DATA generateData(camPackage::CAM cam);
+
+	/** Receives new GPS data from the GPS module.
+	 *
+	 */
+	void receiveGpsData();
+
+	/** Receives new OBD2 data from the OBD2 module.
+	 *
+	 */
+	void receiveObd2Data();
+
 	GlobalConfig mGlobalConfig;
 	CaServiceConfig mConfig;
 
@@ -73,10 +138,16 @@ private:
 	double mCamTriggerInterval;
 
 	gpsPackage::GPS mLatestGps;
+	/**
+	 * True iff a GPS was received and it is not too old.
+	 */
 	bool mGpsValid;		//true if GPS was received and it's not too old
 	std::mutex mMutexLatestGps;
 
 	obd2Package::OBD2 mLatestObd2;
+	/**
+	 * True iff a OBD2 was received and it is not too old.
+	 */
 	bool mObd2Valid;
 	std::mutex mMutexLatestObd2;
 
