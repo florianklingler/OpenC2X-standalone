98cffca1d49b1dba457346c0bfd76b6ee5bdf99b
Jan Tiemann <jantie@mail.upb.de>
2016-07-14 20:49:42 +0200

    added config unifying hint todo

--- common/utility/LoggingUtility.h
+++ common/utility/LoggingUtility.h
@@ -19,6 +19,9 @@ public:
 	 * name will log into the same file.
 	 * Experiment Number will be put into the logging file name.
 	 *
+	 * @todo unify all configs into a singel global conifg.
+	 * Shouldn't be that hard as the individual configs use their owners name in the root xmltag.
+	 *
 	 * @param moduleName Module name
 	 * @param expNo Experiment Number
 	 */
