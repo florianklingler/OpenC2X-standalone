4703d1214afa7a5b5b3b70b8595ef60f73cb0624
Stefan Schneider <stschn@mail.upb.de>
2016-07-20 14:56:44 +0200

    License info for DCC

--- dcc/src/dcc.h
+++ dcc/src/dcc.h
@@ -1,3 +1,26 @@
+// This file is part of OpenC2X.
+//
+// OpenC2X is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenC2X is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenC2X.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Authors:
+// Sven Laux <slaux@mail.uni-paderborn.de>
+// Gurjashan Singh Pannu <gurjashan.pannu@ccs-labs.org>
+// Stefan Schneider <stefan.schneider@ccs-labs.org>
+// Jan Tiemann <janhentie@web.de>
+// Florian Klingler <klingler@ccs-labs.org>
+
+
 #ifndef DCC_H_
 #define DCC_H_
 
