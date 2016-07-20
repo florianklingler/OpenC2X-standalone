7ce929cd463db20850e8d04e44575ae7f2946644
Jan Tiemann <jantie@mail.upb.de>
2016-07-20 15:33:07 +0200

    added extensive list of licence information to monitor site, added a link to it to the docu, added licence header to .js files

--- webSite/script/main.js
+++ webSite/script/main.js
@@ -1,3 +1,21 @@
+// This file is part of OpenC2X.
+//
+// OpenC2X is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenC2X is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenC2X.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Authors:
+// Jan Tiemann <janhentie@web.de>
+
 /** @addtogroup website
  *  @{
  */
