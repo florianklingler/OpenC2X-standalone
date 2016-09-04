b58378bd034d850e84fdf5557aa4df387d627f1f
Gurjashan S. Pannu <gspannu@mail.uni-paderborn.de>
2016-09-04 23:54:14 +0200

    add script to compile asn1 to c structures

---
+++ common/asn1/generate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+asn1c -fnative-types -gen-PER -pdu=CAM ITS_CAM_v1.3.2.asn
+
+find . -type l | xargs rm 
+
+#cp /usr/share/asn1c/*.c .
+#cp /usr/share/asn1c/*.h .
+
+#rm converter-sample.c
