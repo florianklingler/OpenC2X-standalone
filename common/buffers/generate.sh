321f98bd149ddbc729c7d1aeb94ef60e16651095
Gurjashan S. Pannu <gspannu@mail.uni-paderborn.de>
2016-10-29 19:20:19 +0200

    fix errors

--- common/buffers/generate.sh
+++ common/buffers/generate.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 mkdir -p build
+cd build
+rm *.pb.cc *.pb.h
+cd ..
+
 protoc --proto_path=./ --cpp_out=build/ cam.proto 
 
 protoc --proto_path=./ --cpp_out=build/ denm.proto
@@ -17,3 +21,27 @@ protoc --proto_path=./ --cpp_out=build/ dccInfo.proto
 protoc --proto_path=./ --cpp_out=build/ camInfo.proto 
 
 protoc --proto_path=./ --cpp_out=build/ ldmData.proto 
+
+protoc --proto_path=./ --cpp_out=build/ BasicContainer.proto
+
+protoc --proto_path=./ --cpp_out=build/ BasicVehicleHighFreqContainer.proto
+
+protoc --proto_path=./ --cpp_out=build/ BasicVehicleLowFreqContainer.proto
+
+protoc --proto_path=./ --cpp_out=build/ CamParameters.proto
+
+protoc --proto_path=./ --cpp_out=build/ CoopAwareness.proto
+
+protoc --proto_path=./ --cpp_out=build/ HighFreqContainer.proto
+
+protoc --proto_path=./ --cpp_out=build/ ItsPduHeader.proto
+
+protoc --proto_path=./ --cpp_out=build/ LowFreqContainer.proto
+
+protoc --proto_path=./ --cpp_out=build/ PathPoint.proto
+
+protoc --proto_path=./ --cpp_out=build/ ProtectedCommunicationZone.proto
+
+protoc --proto_path=./ --cpp_out=build/ RsuHighFreqContainer.proto
+
+protoc --proto_path=./ --cpp_out=build/ SpecialVehicleContainer.proto
