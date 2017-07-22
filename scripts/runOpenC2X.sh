8c6277a2a7a55b4aa32c6f41fac00cbc8b6f59b3
Gurjashan S. Pannu <gspannu@mail.uni-paderborn.de>
2017-07-22 20:55:51 +0200

    run script

--- scripts/runOpenC2X.sh
+++ scripts/runOpenC2X.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-# This script assumes that OpenC2X source is organised in the tree structure like this:
-#.
-#├── build
-#└── src
-#    ├── CMakeLists.txt
-#    ├── cam
-#    ├── common
-#    ├── dcc
-#    ├── denm
-#    ├── denmApp
-#    ├── gps
-#    ├── httpServer
-#    ├── ldm
-#    ├── obd2
-#    └── kernel-patches
+# This script assumes that instructions in OpenC2X/README.txt have been followed
+
+SESSION=OpenC2X
+CURR_DIR=$(pwd)
+OPENC2X=$CURR_DIR/..
+BUILD_DIR=$OPENC2X/build/
+GLOBAL_CONFIG=$OPENC2X/common/config/config.xml
+
+LOCAL_CONFIG_RELATIVE=config/config.xml
+LOGGING_CONF=config/logging.conf
+STATISTICS_CONF=config/statistics.conf
 
 tmux -2 new-session -d -s $SESSION
 
@@ -25,34 +21,35 @@ tmux new-window -t $SESSION:1 -n 'App'
 tmux split-window -h
 tmux select-pane -t 0
 
-for APP in cam httpServer
-do
-    tmux send-keys "cd ../../build/binaries/$APP/bin" C-m
-    tmux send-keys "./$APP" C-m
-    tmux split-window -v
-done 
+tmux send-keys "cd $BUILD_DIR/cam/src" C-m
+tmux send-keys "./cam $GLOBAL_CONFIG $OPENC2X/cam/$LOCAL_CONFIG_RELATIVE $OPENC2X/cam/$LOGGING_CONF $OPENC2X/cam/$STATISTICS_CONF" C-m
+tmux split-window -v
 
-tmux send-keys "cd ../../build/binaries/ldm/bin" C-m
-tmux send-keys "rm ../db/ldm-1.db" C-m
-tmux send-keys "./ldm" C-m
+tmux send-keys "cd $BUILD_DIR/httpServer/src" C-m
+tmux send-keys "./httpServer $GLOBAL_CONFIG $OPENC2X/httpServer/$LOCAL_CONFIG_RELATIVE $OPENC2X/httpServer/$LOGGING_CONF $OPENC2X/httpServer/$STATISTICS_CONF" C-m
+tmux split-window -v
+
+tmux send-keys "cd $BUILD_DIR/ldm/src" C-m
+tmux send-keys "rm ../db/ldm-*.db" C-m
+tmux send-keys "./ldm $GLOBAL_CONFIG $OPENC2X/ldm/$LOGGING_CONF $OPENC2X/ldm/$STATISTICS_CONF" C-m
 tmux split-window -v
 
 tmux kill-pane
 tmux select-pane -t 3
 
-tmux send-keys "cd ../../build/binaries/denm/bin" C-m
-tmux send-keys "./denm" C-m
+tmux send-keys "cd $BUILD_DIR/denm/src" C-m
+tmux send-keys "./denm $GLOBAL_CONFIG $OPENC2X/denm/$LOGGING_CONF $OPENC2X/denm/$STATISTICS_CONF" C-m
 tmux split-window -v
 
-tmux send-keys "cd ../../build/binaries/dcc/bin" C-m
-tmux send-keys "sudo ./dcc" C-m
+tmux send-keys "cd $BUILD_DIR/dcc/src" C-m
+tmux send-keys "sudo ./dcc $GLOBAL_CONFIG $OPENC2X/dcc/$LOCAL_CONFIG_RELATIVE $OPENC2X/dcc/$LOGGING_CONF $OPENC2X/dcc/$STATISTICS_CONF" C-m
 tmux split-window -v
 
-tmux send-keys "cd ../../build/binaries/obd2/bin" C-m
-tmux send-keys "./obd2" C-m
+tmux send-keys "cd $BUILD_DIR/obd2/src" C-m
+tmux send-keys "./obd2 $GLOBAL_CONFIG $OPENC2X/obd2/$LOCAL_CONFIG_RELATIVE $OPENC2X/obd2/$LOGGING_CONF $OPENC2X/obd2/$STATISTICS_CONF" C-m
 tmux split-window -v
 
-tmux send-keys "cd ../../build/binaries/gps/bin" C-m
-tmux send-keys "./gpsService" C-m
+tmux send-keys "cd $BUILD_DIR/gps/src" C-m
+tmux send-keys "./gpsService $GLOBAL_CONFIG $OPENC2X/gps/$LOCAL_CONFIG_RELATIVE $OPENC2X/gps/$LOGGING_CONF $OPENC2X/gps/$STATISTICS_CONF" C-m
 
 tmux -2 attach-session -t $SESSION
